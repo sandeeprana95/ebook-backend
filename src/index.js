7206533285100663962b9c5c866d5cdc26e75c4b
sandeeprana95 <sandeeprana385@gmail.com>
2025-07-03 16:27:07 +0530

    Moved .env to root and check db connection

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ dotenv.config()
 
 import mongoose from "mongoose"
 mongoose.connect(process.env.DB_URL)
+.then(()=>console.log("db connected"))
+.catch((err)=>console.log(err.message))
 
 import express from "express"
 const app = express()
