3b53a3b4f6e8a3c7014dab2b94f7e979a6af4b5b
sandeeprana95 <sandeeprana385@gmail.com>
2025-07-03 21:56:38 +0530

    add async error handler utility and wrap createUser controller

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,16 +1,11 @@
 import UserModel from "./user.model.js"
+import Exc from "../util/exc.util.js"
+const x = Exc()
+console.log(x)
 	
- export const createUser = async(req,res)=>{
-	 try{
+ export const createUser =Exc(async(req,res)=>{
 		const user = new UserModel(req.body)
 		await user.save()
 		res.json({message:"signup success"})
-
-	 }
-	 catch(err)
-	 {
-		res.status(500).json({
-			message:err.message
-		})
-	 }
-}
+	
+})
---
+++ src/util/exc.util.js
@@ -0,0 +1,9 @@
+const Exc=(fn)=>(req,res,next)=>{
+    Promise.resolve(fn(req,res,next)).catch((err)=>{
+        res.status(500).json({
+            message:process.env.NODE_ENV === "dev"? err.message : "failed! Please try after sometime"
+        })
+    })
+}
+
+export default Exc
