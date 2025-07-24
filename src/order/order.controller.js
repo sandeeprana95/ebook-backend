62b185893439e659c149871cbe0826a9a3f09bf1
sandeeprana95 <sandeeprana385@gmail.com>
2025-07-24 21:39:15 +0530

    feat:order pagination

--- src/order/order.controller.js
+++ src/order/order.controller.js
@@ -14,18 +14,33 @@ import { fetchPaymentById } from "../payment/payment.controller.js"
          return res.json(orders)
 	 }
 	 
-	 else
-     orders = await OrderModel.find().sort({createdAt:-1}).populate("ebook").populate("user","email fullname mobile -_id").lean()
-	 
-		for(let order of orders){
-			const payment = await fetchPaymentById(order.paymentId)
-			modified.push({
-				...order,
-				payment
-			})
-		}     
+	 else{
+		const page = Number(req.query.page)? Number(req.query.page) : 1
+		const limit = Number(req.query.limit)? Number(req.query.limit) : 6
 
-	 res.json(modified)
+		const skip = (page-1)*limit
+		orders = await OrderModel.find().sort({createdAt:-1})
+		.skip(skip)
+		.limit(limit)
+		.populate("ebook","title -_id")
+		.populate("user","email fullname mobile -_id")
+		.lean()
+		
+			for(let order of orders){
+				const payment = await fetchPaymentById(order.paymentId)
+				modified.push({
+					...order,
+					payment
+				})
+			} 
+			
+		const total = await OrderModel.countDocuments()	
+
+		res.json({
+			total,
+			data:modified
+	       })
+	 }
 
 })
 
